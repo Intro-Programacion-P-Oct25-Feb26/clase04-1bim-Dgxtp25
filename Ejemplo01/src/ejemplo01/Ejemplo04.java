a6d3875c71faef814517d5f52803133c64356290
Dgxtp25 <dgxtpbr@gmail.com>
2025-10-29 12:12:18 -0500

    Commit6

--- Ejemplo01/src/ejemplo01/Ejemplo04.java
+++ Ejemplo01/src/ejemplo01/Ejemplo04.java
@@ -18,7 +18,7 @@ public class Ejemplo04 {
         System.out.print(apellidoEstudiante + " ");
         System.out.print(nacimiento + " ");
         System.out.print(nombreEstudiante + " ");
-        //Print se diferencia de println en que println baja un espacio y print se mantiene en esa linea
+        //Print se diferencia de println en que println baja un espacio y print se mantiene en esa linea, asi mismo printl hace un salto de linea
         System.out.println("----------------");
         System.out.println(nombreEstudiante + " ");
         System.out.println(apellidoEstudiante + " ");
