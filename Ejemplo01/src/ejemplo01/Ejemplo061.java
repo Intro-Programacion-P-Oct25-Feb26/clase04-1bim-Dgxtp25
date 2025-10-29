8a682fff5c656e5bc23b0ce74132612470972663
Dgxtp25 <dgxtpbr@gmail.com>
2025-10-29 12:51:00 -0500

    Commit9

---
+++ Ejemplo01/src/ejemplo01/Ejemplo061.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo061 {
+    public static void main(String[] args) {
+        // Uso de printf
+        // f: formato
+        
+        String nombreEstudiante = "Diego Bernabe";
+        String apellidoEstudiante = "Ortiz ";
+        int edad = 19;
+        String direccion = "Avenida Exodo de Yangana";
+        String universidad = "UTPL";
+        String ciclo = "Primer ciclo";
+        String asignatura = "Computacion";
+        String paralelo = "Paralelo A";
+        
+        
+        // System.out.println(nombreEstudiante+" "+apellidoEstudiante+ " "+ 
+        // nacimiento);
+        // En printf; 
+        // usamos %s para reemplazar las cadenas, enteros, decimales
+        // usamos %d para reemplazar los valores enteros
+        // usamos %f para reemplazar los valore decimales
+        // System.out.printf("%s\n\n%s\n\n%s\n", nombreEstudiante, 
+        //        apellidoEstudiante, nacimiento);
+        System.out.printf("Nombre:%s\n\n%s\n\n%s\n", nombreEstudiante, 
+                apellidoEstudiante, edad, direccion, universidad, ciclo, asignatura, paralelo);
+        System.out.printf("Nombre:%s\n\n%s\n\n%d\n", nombreEstudiante, 
+                apellidoEstudiante, edad, direccion, universidad, ciclo, asignatura, paralelo);
+        System.out.printf("Nombre:\n\t%s\napellido:n\t%s%s", 
+    
+
